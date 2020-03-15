576b2c8dd1edfe68b17b48259773c0bda8f83dd5
Vladimir <vladimir@safehouse.ro>
2020-03-15 17:17:59 +0200

    primele doua pagini web, doar designul este implementat

---
+++ public/js/home.js
@@ -0,0 +1,18 @@
+// let displayed = false;
+// document.getElementById("btnShowPhoneMenu").addEventListener('click', ()=>{
+//     if(displayed == false){
+//     document.getElementById('interactMenuPhone').classList.add('display');
+//     document.getElementById('interactMenuPhone').classList.remove('hide');
+//     displayed = true;
+//     }
+//     else {
+//         document.getElementById('interactMenuPhone').classList.add('hide');
+//         document.getElementById('interactMenuPhone').classList.remove('display');
+//         displayed = false;
+//     }
+// })
+
+document.getElementById('contactForm').addEventListener('submit', (e)=>{
+    e.preventDefault()
+    console.log('heeei')
+})
