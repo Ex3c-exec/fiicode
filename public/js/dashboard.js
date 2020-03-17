74bb743132ee28979cb58777e4b1a83f53cda302
Vladimir <vladimir@safehouse.ro>
2020-03-17 16:57:48 +0200

    front end design p3

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,14 +1,29 @@
-// COMPONENT SHOW SYSTEM // AFISARE TABURI
+// COMPONENT SHOW SYSTEM // AFISARE COMPONENTA 
 function componentSelector(componentID){
-    const components = ['single-book-component', 'multiple-books-component'];
+    const components = ['single-book-component', 'multiple-books-component', 'request-component', 'account-component'];
     
     componentID = componentID.replace('btn','component')
-    console.log(componentID)
     components.forEach(component=>{
-        if(component == componentID)
+        if(component == componentID) {
             document.getElementById(component).style.display = 'block';
+            window.location.replace('#' + component);
+            // window.scrollTo(0, 0);
+        }
         else
             document.getElementById(component).style.display = 'none';
     })
-    }
-    document.addEventListener('DOMContentLoaded', componentSelector('multiple-books-component'));
+}
+document.addEventListener('DOMContentLoaded', componentSelector('multiple-books-component'));
+
+// VERIFICAM DACA HREF-UL S-A SCHIMBAT
+window.addEventListener('hashchange', ()=>{
+    let href = window.location.hash;
+    if(href.charAt(0) == '#')
+        href = href.substr(1);
+    componentSelector(href)
+})
+
+//  SCHIMBAM HREF-UL
+const changeHref = (href)=>{
+    window.location.replace('#' + href)
+}
