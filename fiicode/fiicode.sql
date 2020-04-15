0a3e062ab9427a1717f1337782813065c4076e0e
Ex3c-exec <58399324+Ex3c-exec@users.noreply.github.com>
2020-04-15 11:20:29 +0300

    Update fiicode.sql

--- fiicode/fiicode.sql
+++ fiicode/fiicode.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 4.9.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 14, 2020 at 07:36 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.2.28
+-- Gazdă: 127.0.0.1
+-- Timp de generare: apr. 15, 2020 la 10:19 AM
+-- Versiune server: 10.4.11-MariaDB
+-- Versiune PHP: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `fiicode`
+-- Bază de date: `fiicode`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `carti`
+-- Structură tabel pentru tabel `carti`
 --
 
 CREATE TABLE `carti` (
@@ -39,17 +39,18 @@ CREATE TABLE `carti` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `carti`
+-- Eliminarea datelor din tabel `carti`
 --
 
 INSERT INTO `carti` (`id`, `title`, `author`, `img`, `description`, `likes`, `available`) VALUES
-(12, 'A Brief History Of Time: From Big Bang To Black Holes', 'Stephen Hawking', 'https://images-na.ssl-images-amazon.com/images/I/51P8miZZ6OL.jpg', '            It begins by reviewing the great theories of the cosmos from Newton to Einstein, before delving into the secrets which still lie at the heart of space and time, from the Big Bang to black holes, via spiral galaxies and strong theory. To this day A Brief History of Time remains a staple of the scientific canon, and its succinct and clear language continues to introduce millions to the universe and its wonders    ', 1, 1),
-(13, 'Ed Futures: A Collection of Short Stories on the Future of Education', 'Stavros Yiannouka ', 'https://images-na.ssl-images-amazon.com/images/I/516kUGLwuTL.jpg', 'Ordered steak. The menu item blinked in red twice in my digital lens but I didn’t care. I was very hungry and I love meat. Since it became possible to merge our genomic data with health records in the cloud synchronized with each and every digital device, choosing food has turned into a stressful activity. They wanted to tell me what and when to eat and it has made me crazy. They knew better what me and especialy.', 0, 1);
+(12, 'A Brief History Of Time: From Big Bang To Black Holes', 'Stephen Hawking', 'https://images-na.ssl-images-amazon.com/images/I/51P8miZZ6OL.jpg', '            It begins by reviewing the great theories of the cosmos from Newton to Einstein, before delving into the secrets which still lie at the heart of space and time, from the Big Bang to black holes, via spiral galaxies and strong theory. To this day A Brief History of Time remains a staple of the scientific canon, and its succinct and clear language continues to introduce millions to the universe and its wonders    ', 0, 1),
+(13, 'Ed Futures: A Collection of Short Stories on the Future of Education', 'Stavros Yiannouka ', 'https://images-na.ssl-images-amazon.com/images/I/516kUGLwuTL.jpg', 'Ordered steak. The menu item blinked in red twice in my digital lens but I didn’t care. I was very hungry and I love meat. Since it became possible to merge our genomic data with health records in the cloud synchronized with each and every digital device, choosing food has turned into a stressful activity. They wanted to tell me what and when to eat and it has made me crazy. They knew better what me and especialy.', 0, 1),
+(14, 'The Count of Monte Cristo', ' Alexandre Dumas', 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1309203605l/7126.jpg', '                                            Thrown in prison for a crime he has not committed, Edmond Dantes is confined to the grim fortress of If. There he learns of a great hoard of treasure hidden on the Isle of Monte Cristo and he becomes determined not only to escape, but also to unearth the treasure and use it to plot the destruction of the three men responsible for his incarceration. Dumas’ epic tale of suffering and retribution, inspired by a real-life case of wrongful imprisonment, was a huge popular success when it was first serialized in the 1845s.\n                        ', 2, 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `likes`
+-- Structură tabel pentru tabel `likes`
 --
 
 CREATE TABLE `likes` (
@@ -59,16 +60,17 @@ CREATE TABLE `likes` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `likes`
+-- Eliminarea datelor din tabel `likes`
 --
 
 INSERT INTO `likes` (`id`, `userid`, `bookid`) VALUES
-(7, 34, 12);
+(15, 39, 14),
+(16, 40, 14);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `password_reset`
+-- Structură tabel pentru tabel `password_reset`
 --
 
 CREATE TABLE `password_reset` (
@@ -79,10 +81,17 @@ CREATE TABLE `password_reset` (
   `expire` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Eliminarea datelor din tabel `password_reset`
+--
+
+INSERT INTO `password_reset` (`id`, `email`, `selector`, `token`, `expire`) VALUES
+(15, 'user@user.com', '72d02824dc9ef21e', '$2y$10$7B1flumsnos7g1eCLWMI1u20U3R0SmCydMFF8Lsl7UzFjCPI0yv/i', '1586940206');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `request`
+-- Structură tabel pentru tabel `request`
 --
 
 CREATE TABLE `request` (
@@ -96,23 +105,16 @@ CREATE TABLE `request` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `request`
+-- Eliminarea datelor din tabel `request`
 --
 
 INSERT INTO `request` (`id`, `phone`, `book`, `date`, `termen`, `email`, `status`) VALUES
-(4, 744233098, 'ion', '2020-03-23', 2, 'martinescunicolaee3@gmail.com', 1),
-(5, 744233098, 'KWFKQW', '2020-03-24', 0, 'admin@gmail.com', 1),
-(6, 744233098, 'KWFKQW', '2020-03-24', 0, 'admin@gmail.com', 1),
-(7, 744233098, 'ion', '2020-03-25', 0, 'martinescunicolaee3@gmail.com', -1),
-(8, 755555555, 'A Brief History Of Time: From Big Bang To Black Holes', '2020-03-25', 0, 'admin@gmail.com', 1),
-(9, 744233098, 'A Brief History Of Time: From Big Bang To Black Holes', '2020-03-25', 4, 'martinescunicolaee3@gmail.com', 1),
-(10, 744233098, 'A Brief History Of Time: From Big Bang To Black Holes', '2020-03-25', 4, 'martinescunicolaee3@gmail.com', 1),
-(11, 744233098, 'Ed Futures: A Collection of Short Stories on the Future of Education', '2020-03-25', 0, 'martinescunicolaee3@gmail.com', 1);
+(13, 730591455, 'A Brief History Of Time: From Big Bang To Black Holes', '2020-04-15', 1, 'user@user.com', -1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Structură tabel pentru tabel `user`
 --
 
 CREATE TABLE `user` (
@@ -126,86 +128,81 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `user`
+-- Eliminarea datelor din tabel `user`
 --
 
 INSERT INTO `user` (`ID`, `first_name`, `last_name`, `email`, `address`, `password`, `admin`) VALUES
-(30, 'Elena', 'Martinescu', 'elena.martinescu@yahoo.com', 'str dis nr 995', '$2y$10$Hrh0ncYnlI9CeUvval9YVOjLu8yW8J3GjPj2JSwLh2aJEXNVbjJYW', 0),
-(31, 'Nicolae', 'Martinescu', 'martinescu@gmail.com', 'strada principala marginea nr995', '$2y$10$I.3ZOsuADdGwTNOY.nWvCeYj18mQHQ/.aH8P2JX6KfsKuShGVxmNC', 0),
-(32, 'Nicolae', 'Martinescu', 'martinescu@gmail.comW', 'strada principala marginea nr995', '$2y$10$9Z25Bhk9jyg1/bZC4dRjReOJVEhopqmaj2W/d5szmHCnWNkjjdChq', 0),
-(33, 'Nicolae', 'Martinescu', 'martinescu.nico@hotmail.com', 'adr', '$2y$10$hcEWqfMyCHEN0HHKdO95jeloYPiO0tFo4zuLi30hfPASwvvSI359C', 0),
-(34, 'Nicolae', 'Martinescu', 'martinescunicolaee3@gmail.com', 'addr', '$2y$10$rv9D.ymJ1brafc7MaBmuYeXymGuFw60Z804E.m1aMT.qV8fymBaqu', 0),
-(35, 'Nicu', 'Martinescu', 'admin@gmail.com', 'addr', '$2y$10$irCGN1ZcAuG1D0uGU/5myeYxNEpswXGpSOPcBTSpV/dc7HoOkXVYC', 1),
-(36, 'nicolae', 'martinescu', 'elena.martinescu@yahoo.comaaa', 'addr', '$2y$10$2UgMuofpBghIimQaxKPKQOTuEsDyLSXNGZJvICQ2rxp7yXDlQjKEC', 0);
+(39, 'admin', 'admin', 'fiicodebooksemail@gmail.com', 'admin street', '$2y$10$k0vawh2q.tEmp7FMwB4LjeIPGEqoboOV3/Zdd1M5./qHb2W//y7kK', 1),
+(40, 'user', 'user', 'user@user.com', 'user street', '$2y$10$S7Co9HPxmdwri8LPfb13ceJfE9xw9862noJI/X6aldGPy8z6i.Vm.', 0);
 
 --
--- Indexes for dumped tables
+-- Indexuri pentru tabele eliminate
 --
 
 --
--- Indexes for table `carti`
+-- Indexuri pentru tabele `carti`
 --
 ALTER TABLE `carti`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `likes`
+-- Indexuri pentru tabele `likes`
 --
 ALTER TABLE `likes`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `password_reset`
+-- Indexuri pentru tabele `password_reset`
 --
 ALTER TABLE `password_reset`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `request`
+-- Indexuri pentru tabele `request`
 --
 ALTER TABLE `request`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `status` (`id`);
 
 --
--- Indexes for table `user`
+-- Indexuri pentru tabele `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`ID`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pentru tabele eliminate
 --
 
 --
--- AUTO_INCREMENT for table `carti`
+-- AUTO_INCREMENT pentru tabele `carti`
 --
 ALTER TABLE `carti`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT pentru tabele `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT pentru tabele `password_reset`
+--
+ALTER TABLE `password_reset`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT pentru tabele `request`
+--
+ALTER TABLE `request`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
--- AUTO_INCREMENT for table `likes`
---
-ALTER TABLE `likes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `password_reset`
---
-ALTER TABLE `password_reset`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `request`
---
-ALTER TABLE `request`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
---
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT pentru tabele `user`
 --
 ALTER TABLE `user`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
